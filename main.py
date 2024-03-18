9c39a0cc0da7fcba8fc366232637e19eb4b1c9d5
Remi Cazelles <remi.cazelles.fr@gmail.com>
2024-03-18 15:46:59 +0100

    base structure app

--- main.py
+++ main.py
@@ -1,50 +1,27 @@
-from fastapi import FastAPI, HTTPException, Request
-from transformers import pipeline
-from fastapi.templating import Jinja2Templates
+from flask import Flask, render_template
 
-# instancier l'application 
-app_remi = FastAPI()
+app= Flask(__name__)
 
-# charger le template de mise en page
-templates = Jinja2Templates(directory="templates")
+@app.route('/')
+def index():
+    title = 'Welcome'
+    _user ="Remi"
+    return render_template('index.html', title=title, _user=_user)
 
-@app_remi.get("/")
-async def read_item(request: Request):      # , id: str
-    return templates.TemplateResponse(
-    request=request, name="acceuil.html", context={"id": id}
-)
-
-
-# charge le modèle entrainé
-reponse_pipeline = pipeline("models/model1")
-
-# décorateur pour associer notre fonction "trouver_reponse" à la méthode HTTP GET
-@app_remi.get("/generer_reponse")
-def generer_reponse(question: str):
-
-    # Utilise le modèle pour produire la réponse
-    try:
-        answer = reponse_pipeline(question=question)
-	# Renvoyer la réponse obtenu avec le modèle au format JSON
-        return {"question": question, "reponse": answer["answer"]}
-
-    # Affiche erreur si problème (code 500 pour "catch-all")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-# lance l'app
-if __name__ == "__main__":
-    import uvicorn
-
-    # Démarre l'app sur un serveur local
-    uvicorn.run(app_remi, host="127.0.0.1", port=8000)
+@app.route('/about')
+def about():
+    title = 'Fixed budget'
+    budget_cat = ['Fixed_charges', 'Food', 'Health', 'Other', 'Sorties', 'Renovation']
+    return render_template('about.html', budget_cat=budget_cat, title = title, user_='')
 
 
+@app.route('/contact_form')
+def contact_form():
+    title='Contact us today'
+    return render_template('contact_form.html',title=title)
 
 
-# Dans le terminal, lancer l'app avec:
-# $ uvicorn main:app --reload
-
-
-# Dans les requirements, installer toutes les dependences et features
-# $ pip install "fastapi[all]"
+@app.route('/new_budget_form')
+def new_budget_form():
+    title='Create new budget'
+    return render_template('new_budget_form.html',title=title)
